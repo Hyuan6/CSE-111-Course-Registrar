4e2afb6e69ccb1886c82c4b1579b29009d6e1e87
unknown <=>
2020-11-01 01:35:16 -0700

    Lofi Audio Stream, Pause + Play Button

---
+++ CourseRegistrar/LegacyRegistrationPage/static/LegacyRegistrationPage/development.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    // $('#play-button').on('click', function(ev) {
+    //   console.log("Button clicked.");
+    //   // player.stopVideo()
+    //   player.playVideo();
+    //   ev.preventDefault();
+   
+    // });
+
+    // var playEmbeddedVideo = true;
+
+    // function toggleVideo(){
+    //     if(playEmbeddedVideo){
+    //         player.playVideo();
+    //     }else{
+    //         player.stopVideo();
+    //     }
+    //     playEmbeddedVideo = !playEmbeddedVideo;
+    // }
+});
+
