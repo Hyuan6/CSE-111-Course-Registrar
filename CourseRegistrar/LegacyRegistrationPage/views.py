d28034451fb71844698b84809f1e2b3b6aee2e03
= <hyuan100@gmail.com>
2020-12-10 01:39:32 -0800

    Fixed all null input edge case

--- CourseRegistrar/LegacyRegistrationPage/views.py
+++ CourseRegistrar/LegacyRegistrationPage/views.py
@@ -21,6 +21,8 @@ def closeConnection(_conn, _dbFile):
         print(e)
 
 def index(request):
+    response = render(request, 'LegacyRegistrationPage/LegacyRegistrationPage.html')
+
     if request.method == 'POST':
         form = request.POST.dict()
         database = f"db.sqlite3"
@@ -29,31 +31,36 @@ def index(request):
                values """
         student_id = form.get('student_id')
 
+        null_count = 0
+
         for index, crn in form.items():
-            if "CRN_" in index and crn != '':
+            if crn == '':
+                null_count += 1
+                continue
+
+            if "CRN_" in index:
                 sql += f"({crn}, {student_id}, 'null'),"
 
         #remove hanging ','
         sql = sql[:-1]
 
-        print(sql)
+        if null_count < 10:
+            conn = openConnection(database)
+            cur = conn.cursor()
+            cur.execute(sql)
+            conn.commit()
 
-        conn = openConnection(database)
-        cur = conn.cursor()
-        cur.execute(sql)
-        conn.commit()
+            for index, crn in form.items():
+                if "CRN_" in index and crn != '':
+                    sql = f"""update RegistrationPage_course
+                            set Seats_avil = Seats_avil - 1,
+                                Act_enrl = Act_enrl + 1
+                            where crn = {crn};"""
+                    cur.execute(sql)
+                    conn.commit()
 
-        for index, crn in form.items():
-            if "CRN_" in index and crn != '':
-                sql = f"""update RegistrationPage_course
-                          set Seats_avil = Seats_avil - 1,
-                              Act_enrl = Act_enrl + 1
-                          where crn = {crn};"""
-                cur.execute(sql)
-                conn.commit()
+            closeConnection(conn, database)
 
-        closeConnection(conn, database)
-
-    return render(request, 'LegacyRegistrationPage/LegacyRegistrationPage.html')
+    return response
 
     
