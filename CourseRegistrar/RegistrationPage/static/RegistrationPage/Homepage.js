71e8b63c365410b2010ef19e1e7f0e30059b16e4
= <hyuan100@gmail.com>
2020-12-10 15:48:43 -0800

    Added user profile page to home page

--- CourseRegistrar/RegistrationPage/static/RegistrationPage/Homepage.js
+++ CourseRegistrar/RegistrationPage/static/RegistrationPage/Homepage.js
@@ -1,7 +1,3 @@
-// function welcome() {
-//     $("#HomePageText").text("Welcome " + $(" #usernameInput ").val())
-// }
-
 $(document).ready(function() {
     setTimeout(function() {
         $("#cookieConsent").fadeIn(200);
@@ -9,4 +5,40 @@ $(document).ready(function() {
     $("#closeCookieConsent, .cookieConsentOK").click(function() {
         $("#cookieConsent").fadeOut(200);
     });
+    updatePage();
 });
+
+function updatePage() {
+    var cookie = getcookie("student_id");
+    if (cookie == undefined) {
+        document.getElementById("Main Banner").style = "display: block;";
+        document.getElementById("User Profile").style = "display: none;";
+    } else {
+        document.getElementById("Main Banner").style = "display: none;";
+        document.getElementById("User Profile").style = "display: block;";
+        populateUserProfile(student_id = cookie);
+    }
+}
+
+function populateUserProfile(student_id = '') {}
+
+function getcookie(name = '') {
+    let cookies = document.cookie;
+    let cookiestore = {};
+
+    cookies = cookies.split(";");
+
+    if (cookies[0] == "" && cookies[0][0] == undefined) {
+        return undefined;
+    }
+
+    cookies.forEach(function(cookie) {
+        cookie = cookie.split(/=(.+)/);
+        if (cookie[0].substr(0, 1) == ' ') {
+            cookie[0] = cookie[0].substr(1);
+        }
+        cookiestore[cookie[0]] = cookie[1];
+    });
+
+    return (name !== '' ? cookiestore[name] : cookiestore);
+}
