877c8e8a87158bbdf9b8e3c67c7f30d53dd0dfed
marco <mharo-crespo@ucmerced.edu>
2020-12-10 02:20:47 -0800

    autocomplete and submit routines done

--- CourseRegistrar/RegistrationPage/static/RegistrationPage/regPage.js
+++ CourseRegistrar/RegistrationPage/static/RegistrationPage/regPage.js
@@ -1,25 +1,108 @@
 $(document).ready(function(){
-    var countries = [
-        { label: 'United Kingdom', value: 'UK' },
-        { label: 'United States', value: 'US' }
-    ];
     
-    var input = document.getElementById("search_box");
     
-    autocomplete({
-        input: input,
-        fetch: function(text, update) {
-            text = text.toLowerCase();
-            // you can also use AJAX requests instead of preloaded data
-            var suggestions = countries.filter(n => n.label.toLowerCase().startsWith(text))
-            update(suggestions);
+    $("#submit").click(function(){
+        var cl = document.getElementsByClassName("course-item");
+        var classes = [];
+        var i;
+        console.log(cl.length)
+        for(i = 0; i < cl.length;i++){
+            var count = i;
+            classes.push({[count]:cl[i].innerText.split('\n')[0].split(' ')[0]})
+        }
+        console.log(classes);
+        $.ajax({
+            type: 'GET',
+            url:"/CourseReg/register/", 
+            data:{
+                classes
+            },
+            success: function(data){
+                alert("registered")
+                console.log("shits done");
+            },
+            failure: function(data){
+                alert("shits fucked");
+            },
+        });
+    });
+    
+    
+    function ac(){
+        var rep = [];
+        $.ajax({
+            type: 'GET',
+            url:"/CourseReg/ajax/",
+            async: false,
+            success: function(data){
+                console.log(data)
+                items = data
+            },
+            failure: function(data){
+                console.log(data)
+                alert("shits fucked");
+            },
+        });
+        rep = items
+        return rep;
+    }
+    var existing_elements = [];
+    function exists(el){
+        return existing_elements.includes(el);
+    }
+
+    new autoComplete({
+        data:{
+            src: ac(),
+            cache: false
         },
-        onSelect: function(item) {
-            input.value = item.label;
-            var new_div = document.createElement("div")
-            new_div.role = "option"
-            document.getElementById("selclasses").appendChild(new_div)
-            console.log(item)
+        placeHolder:"Course Number",
+        highlight:true,
+        searchEngine: "loose",
+        onSelection: feedback =>{
+            if(!exists(feedback.selection.value)){
+                var new_div = document.createElement("div")
+                new_div.className = "course-item"
+                // add buttons and inner div
+                new_div.innerHTML = feedback.selection.value + " : <div class='course-list-options'><button class='ui grey tiny button' role='button'>Sections</button><button class='ui pink tiny icon button' role='button'><i aria-hidden='true' class='close icon'></i></button></div>"
+                document.getElementById("selclasses").appendChild(new_div)
+                existing_elements.push(feedback.selection.value);
+            }
+            else{
+                alert("already added")
+            }
+            
         }
     });
+    
+    
+    
+
+
+
+
+
+
+    // autocomplete({
+    //     input: input,
+    //     fetch: function(text, update) {
+    //         text = text.toLowerCase();
+    //         // you can also use AJAX requests instead of preloaded data
+    //         var suggestions = countries.filter(n => n.label.toLowerCase().startsWith(text))
+    //         console.log(suggestions)
+    //         update(suggestions);
+    //     },
+    //     onSelect: function(item) {
+    //         input.value = item.label;
+    //         //create new div
+    //         var new_div = document.createElement("div")
+    //         new_div.className = "course-item"
+    //         // add buttons and inner div
+    //         new_div.innerHTML = item.label + ": <div class='course-list-options'><button class='ui grey tiny button' role='button'>Sections</button><button class='ui pink tiny icon button' role='button'><i aria-hidden='true' class='close icon'></i></button></div>"
+    //         document.getElementById("selclasses").appendChild(new_div)
+            
+    //         //THIS DOES NOT CHECK IF CLASS ALREADY ADDED
+            
+    //     }
+    // });
 });
